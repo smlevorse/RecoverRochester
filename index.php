bab2f656ae49361a646dd032c71dfd3e243749d9
Mars <mars.ballantyne@gmail.con>
2015-11-14 18:17:05 -0500

    updated login page

--- index.php
+++ index.php
@@ -3,12 +3,21 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>FCN |  Food Circulation Network<title>
+        <title>FCN |  Food Circulation Network</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="css/normalize.min.css">
         <link rel="stylesheet" href="css/main.css">
+
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+
+        <style type="text/css">
+
+        
+
+
+        </style>
 
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
@@ -18,6 +27,12 @@
         <![endif]-->
 
         <p>Hello world! This is HTML5 Boilerplate.</p>
+
+        <div class="pure-g">
+        <div class="pure-u-1-3"><p>Thirds</p></div>
+        <div class="pure-u-1-3"><p>Thirds</p></div>
+        <div class="pure-u-1-3"><p>Thirds</p></div>
+        </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.js"><\/script>')</script>
